e2b36978b87f13fe6ed73b157eb3a082562c8013
IlanVaks <ilanvaks@gmail.com>
2023-03-24 10:20:46 -0400

    did basic loops

--- arrays.js
+++ arrays.js
@@ -2,8 +2,9 @@
 
 const fruits = ['apple', 'banana', 'orange', 'kiwi', 'grapes']
 
-// write loop here...
-
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i])
+}
 
 // Given the following array, output each student's first name
 
@@ -23,11 +24,10 @@ const students = [
     lastName: 'Stamper',
     gender: 'female',
   },
-  {
-    firstName: 'Meghann',
-    lastName: 'Stamper',
-    gender: 'female',
-  },
 ]
 
-// write loop here....
+for (i = 0; i < students.length; i++) {
+  console.log(students[i].firstName)
+}
+
+//.length same thing as saying 3 since 3 student objects in the array
